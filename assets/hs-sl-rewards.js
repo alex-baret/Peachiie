0d329e42e85d0bdf080e36a339fc195cf8d344a1
Alex Baret <alexbaret7@icloud.com>
2020-10-15 13:52:41 -0600

    Add all initial theme files

---
+++ assets/hs-sl-rewards.js
@@ -0,0 +1,3 @@
+window.HSSLIDECART = window.HSSLIDECART || {};
+window.HSSLIDECART = window.HSSLIDECART || {};
+window.HSSLIDECART.rewards = { json:[{"id":"244","amount":"150.00","title":"{{ price }} away from free shipping!","country":"United States","country_small":"US","customer_id":"1071"}] }
